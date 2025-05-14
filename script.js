bb11680e6ee8a22609c45f8c44eb9f0c6fa4b91c
Ismail Yildirim <93192715+Ismailcny@users.noreply.github.com>
2025-05-15 00:19:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 setInterval(function() {
-  fetch('https://api.instagram.com/v1/users/self')
-    .then(response => response.json())
-    .then(data => {
-      document.getElementById('followers').textContent = data.data.counts.followed_by;
-    })
-    .catch(err => console.error('Error fetching followers:', err));
-}, 5000);  // Every 5 seconds
+    fetch('data/followers.json')  // This assumes the followers.json file is inside a "data" folder.
+      .then(response => response.json())
+      .then(data => {
+          document.getElementById('followers').textContent = data.followers_count;
+      })
+      .catch(err => console.error('Error fetching followers:', err));
+}, 5000);  // Update every 5 seconds
