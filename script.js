8be6f41bb9593125f8f4b5182c94ec3919b31c86
Ismail Yildirim <93192715+Ismailcny@users.noreply.github.com>
2025-05-15 02:11:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-setInterval(function() {
-    fetch('data/followers.json')  // This assumes the followers.json file is inside a "data" folder.
-      .then(response => response.json())
-      .then(data => {
-          document.getElementById('followers').textContent = data.followers_count;
-      })
-      .catch(err => console.error('Error fetching followers:', err));
-}, 5000);  // Update every 5 seconds
